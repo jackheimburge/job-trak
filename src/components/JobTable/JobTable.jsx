49a60e57f266b7df51d11c7d9beabcb6b195487e
centipedejam <jheimburge@gmail.com>
2023-11-07 12:21:28 -0800

    set up table and tablerow components

--- src/components/JobTable/JobTable.jsx
+++ src/components/JobTable/JobTable.jsx
@@ -1,9 +1,39 @@
+import TableRow from '../TableRow/TableRow';
 import './JobTable.css';
+
 
 export default function JobTable() {
     return (
-        <div className='JobTable'>
-            JobTable
+        <div className='TableDiv'>
+            <table className='JobTable'>
+                <thead>
+                    <tr>
+                        <th>Date</th>
+                        <th>Status</th>
+                        <th>Title</th>
+                        <th>Company</th>
+                        <th>Type</th>
+                        <th>Location</th>
+                        <th>Salary</th>
+                        <th>Description</th>
+                        <th>Suitability</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <TableRow />
+                    <tr>
+                        <td>11/8/2023</td>
+                        <td>Denied</td>
+                        <td>Python Developer</td>
+                        <td>Spotify</td>
+                        <td>Remote</td>
+                        <td>N/A</td>
+                        <td>$200,000</td>
+                        <td>Backend Python developer role with spotify</td>
+                        <td>⭐️</td>
+                    </tr>
+                </tbody>
+            </table>
         </div>
     );
 }
---
+++ src/components/TableRow/TableRow.jsx
@@ -0,0 +1,17 @@
+
+
+export default function TableRow() {
+    return (
+        <tr>
+            <td>11/7/2023</td>
+            <td>Applied</td>
+            <td>Frontend Developer</td>
+            <td>MLB</td>
+            <td>Hybrid</td>
+            <td>San Diego, CA</td>
+            <td>$85,000</td>
+            <td>Frontend react.js job with the San Diego Padres</td>
+            <td>⭐️⭐️⭐️⭐️</td>
+        </tr>
+    );
+}
