f6b8a7d89b88868741eac5c9a432221e8410e922
centipedejam <jheimburge@gmail.com>
2023-11-12 18:44:40 -0800

    set sort order and suitability sort

--- src/components/JobTable/JobTable.jsx
+++ src/components/JobTable/JobTable.jsx
@@ -1,12 +1,32 @@
+import { useState } from 'react';
 import TableRow from '../TableRow/TableRow';
 import NewEntryRow from '../NewEntryRow/NewEntryRow';
 import './JobTable.css';
 
 
 export default function JobTable({ jobs, setJobs }) {
+    const [sortOrder, setSortOrder] = useState('asc');
     function handleSalaryClick() {
-        const sortedJobs = [...jobs].sort((a, b) => a.salary - b.salary);
+        const sortedJobs = [...jobs].sort((a, b) => {
+            if (sortOrder === 'asc') {
+                return a.salary - b.salary;
+            } else {
+                return b.salary - a.salary;
+            }
+        });
         setJobs(sortedJobs);
+        setSortOrder(sortOrder === 'asc' ? 'desc' : 'asc');
+    }
+    function handleSuitabilityClick() {
+        const sortedJobs = [...jobs].sort((a, b) => {
+            if (sortOrder === 'asc') {
+                return a.suitability - b.suitability;
+            } else {
+                return b.suitability - a.suitability;
+            }
+        });
+        setJobs(sortedJobs);
+        setSortOrder(sortOrder === 'asc' ? 'desc' : 'asc');
     }
     return (
         <div className='TableDiv'>
@@ -22,7 +42,7 @@ export default function JobTable({ jobs, setJobs }) {
                         <th>Location</th>
                         <th onClick={handleSalaryClick}>Salary</th>
                         <th>URL</th>
-                        <th>Suitability</th>
+                        <th onClick={handleSuitabilityClick}>Suitability</th>
                         <th></th>
                     </tr>
                 </thead>
