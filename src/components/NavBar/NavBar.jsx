4ea6d8f9d840620c0c8a4d3005418a727fec0a92
centipedejam <jheimburge@gmail.com>
2023-11-17 23:29:55 -0800

    basic styling for dropdown nav

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,9 +1,14 @@
 import { Link } from 'react-router-dom';
+import { useState } from 'react';
 import * as userService from '../../utilities/users-service';
 import './Navbar.css'
 
 export default function NavBar({ user, setUser }) {
+    const [dropdownIsActive, setDropdownIsActive] = useState(false);
 
+    function toggleDropdown() {
+        setDropdownIsActive(!dropdownIsActive);
+    }
     function handleLogOut() {
         userService.logOut();
         setUser(null);
@@ -11,11 +16,13 @@ export default function NavBar({ user, setUser }) {
 
     return (
         <nav>
-            <span>Welcome, {user.name}</span>
-            &nbsp; | &nbsp;
-            <Link to="/dashboard" >My Dashboard</Link>
-            &nbsp; | &nbsp;
-            <Link to="" onClick={handleLogOut}>Log Out</Link>
+            <span onClick={toggleDropdown}>Welcome, {user.name} &#8691;</span>
+            {dropdownIsActive && (
+                <div className='dropdown-content'>
+                    <Link to="/dashboard" >My Dashboard</Link>
+                    <Link to="" onClick={handleLogOut}>Log Out</Link>
+                </div>
+            )}
         </nav>
     );
 }
