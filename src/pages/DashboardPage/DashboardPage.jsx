4b75e8a4ba319fb3d1c46d36031cfedaaa0e25a4
centipedejam <jheimburge@gmail.com>
2023-12-03 14:42:47 -0800

    fix prevmonth logic

--- src/pages/DashboardPage/DashboardPage.jsx
+++ src/pages/DashboardPage/DashboardPage.jsx
@@ -44,7 +44,7 @@ export default function DashboardPage({ jobs }) {
                 <div>Rejections: {jobsCurMonth.filter((job) => job.status === 'Rejected').length}</div>
             </div>
             <div className='prevMonth'>
-                <h2>Analytics for {months[curMonth - 2]},&nbsp; {curYear}</h2>
+                <h2>Analytics for {months[curMonth - 2]},&nbsp; {prevYear}</h2>
                 <div>Applications: {jobsPrevMonth.length}</div>
                 <div>Rejections: {jobsPrevMonth.filter((job) => job.status === 'Rejected').length}</div>
             </div>
