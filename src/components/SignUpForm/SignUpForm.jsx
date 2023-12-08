4cfd1684874218cffc3cd9c96c9d12ca1569ff0e
centipedejam <jheimburge@gmail.com>
2023-12-08 11:13:14 -0800

    client side validation on sign up

--- src/components/SignUpForm/SignUpForm.jsx
+++ src/components/SignUpForm/SignUpForm.jsx
@@ -20,8 +20,30 @@ export default class SignUpForm extends Component {
   handleSubmit = async (evt) => {
     evt.preventDefault();
     try {
-      const { name, email, password, username } = this.state
+      const { name, email, password, username, confirm } = this.state
       const formData = { name, email, password, username };
+      if (!/^[a-zA-Z-' ]*$/.test(name)) {
+        this.setState({ error: 'Invalid name' });
+        return;
+      }
+
+      // Email validation
+      if (!/\S+@\S+\.\S+/.test(email)) {
+        this.setState({ error: 'Invalid email' });
+        return;
+      }
+
+      // Password validation
+      if (password.length < 8 || !/[A-Z]/.test(password) || !/[a-z]/.test(password) || !/[0-9]/.test(password) || !/[^A-Za-z0-9]/.test(password)) {
+        this.setState({ error: 'Password must be at least 8 characters long and include an uppercase letter, a lowercase letter, a number, and a special character' });
+        return;
+      }
+
+      // Confirm password validation
+      if (password !== confirm) {
+        this.setState({ error: 'Passwords do not match' });
+        return;
+      }
 
       const user = await signUp(formData);
       this.props.setUser(user);
